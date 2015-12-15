0bd1264468f08261ea0440a6098cf53a1617834a
grammargeek <mlopez.de@mysummitps.org>
2015-12-15 19:27:27 +0000

    NO OVERLAP SHOWN WHEN YOU INPUT THE COLORS!

--- Christmastree.js
+++ Christmastree.js
@@ -40,14 +40,17 @@ var hasOtherOrnaments = false;
 var button12 = document.getElementById("button12");
 var hasPresents = false; 
 
+
+
 function setup() {
     //creates canvas
     createCanvas(1300, 700);
+    // mySound.setVOlume(0.1);
 
     //sets brown for stump
-    var colors = color('brown');
-    fill(colors);
-    var stump = rect(615, 360, 70, 70);
+    // var colors = color('brown');
+    // fill(colors);
+    // var stump = rect(615, 360, 70, 70);
 
     //changes color of stump
     button3.addEventListener("click", function() {
@@ -58,42 +61,42 @@ function setup() {
 
     //sets color for tree
 
-    var c = color('green');
-    fill(c);
-      var tri3 = triangle(460, 370, 650, 200, 830, 370);
-          var tri2 = triangle(460, 310, 650, 190, 820, 310);
-    var tri = triangle(478, 250, 650, 130, 800, 250);
+    // var c = color('green');
+    // fill(c);
+    //   var tri3 = triangle(460, 370, 650, 200, 830, 370);
+    //       var tri2 = triangle(460, 310, 650, 190, 820, 310);
+    // var tri = triangle(478, 250, 650, 130, 800, 250);
 
   
 
     //Changes color for first segment
     button4.addEventListener("click", function() {
-        var color1 = true;
-        var firstColor = color(input4.value);
-        fill(firstColor);
-        var tri = triangle(478, 250, 650, 130, 800, 250);
+        // var color1 = true;
+        // var firstColor = color(input4.value);
+        // fill(firstColor);
+        // var tri = triangle(478, 250, 650, 130, 800, 250);
     });
 
    
 
     //changes color and draws middle section of the tree. 
     button2.addEventListener("click", function() {
-        var c = color(input2.value);
-        fill(c);
-        var tri2 = triangle(460, 310, 650, 190, 820, 310);
+        // var c = color(input2.value);
+        // fill(c);
+        // var tri2 = triangle(460, 310, 650, 190, 820, 310);
 
     });
-   // Used to draw and change color of the stump. 
+   // Used to draw and change color of third section 
     button5.addEventListener("click", function() {
-        var thirdColor = color(input5.value);
-        fill(thirdColor);
-        var tri3 = triangle(460, 370, 650, 200, 830, 370);
+        // var thirdColor = color(input5.value);
+        // fill(thirdColor);
+        // var tri3 = triangle(460, 370, 650, 200, 830, 370);
     });
     //sets topper color
-    var topperColor = color("silver");
-    fill(topperColor);
-    // //creates topper
-    var topper = ellipse(650, 120, 40, 40);
+    // var topperColor = color("silver");
+    // fill(topperColor);
+    // // //creates topper
+    // var topper = ellipse(650, 120, 40, 40);
     button6.addEventListener("click", function() {
         var diffColor = color(input6.value);
         fill(diffColor);
@@ -110,13 +113,7 @@ button1.addEventListener("click", function() {
 //function for star image
     button7.addEventListener("click", function(){
         var hasStar = true;
-        if(hasStar === true){
-            var starTopper = loadImage("startopper.png");
-            imageSprite = createSprite(650, 120);
-            imageSprite.addImage(starTopper);
-            drawSprites();
-            
-        }
+        
         
         
         
@@ -125,30 +122,20 @@ button1.addEventListener("click", function() {
     //used for second star.
     button8.addEventListener("click", function(){
             var hasBlueStar = true;
-            if(hasBlueStar === true){
-                var blueStarTopper = loadImage("star2.png");
-                imageSprite = createSprite(650, 120);
-                imageSprite.addImage(blueStarTopper);
-                drawSprites();
-            }
+           
             
         });
         
         //Used to create the sheep topper.
         button9.addEventListener("click", function(){
             var hasSheep = true;
-            if(hasSheep === true){
-                var sheepTopper = loadImage("sheep.png");
-                imageSprite = createSprite(650, 120);
-                imageSprite.addImage(sheepTopper);
-                drawSprites();
-            }
+            
         });
         
         //Ornaments
         button10.addEventListener("click", function(){
             var hasOrnaments = true;
-            if(hasOrnaments === true){
+             if(hasOrnaments === true){
               
                 var ornament = loadImage("xmas-ball-ornament.png");
                 imageSprite = createSprite(630, 200);
@@ -162,14 +149,13 @@ button1.addEventListener("click", function() {
                 imageSprite = createSprite(580, 320);
                 imageSprite.addImage(ornament);
                 drawSprites();
-            }
+            } 
         });
         
         button11.addEventListener("click", function(){
-            var hasOtherOrnaments = true;
+            var hasOtherOrnaments = true; 
             if(hasOtherOrnaments === true){
-              
-                var ornament = loadImage("sheep.1.png");
+           var ornament = loadImage("sheep.1.png");
                 imageSprite = createSprite(630, 200);
                  imageSprite.addImage(ornament);
                 imageSprite = createSprite(670, 250);
@@ -187,18 +173,16 @@ button1.addEventListener("click", function() {
         //presents
         button12.addEventListener("click", function(){
             var hasPresents = true; 
-            if(hasPresents === true){
-                var presents = loadImage("present.png");
-                imageSprite = createSprite(570, 420);
-                imageSprite.addImage(presents);
-                drawSprites();
-            }
-        }
-        )
-    
-function draw(){
+           }
+        );
+                 
             
-}
+            
+            
+//  function preload() {
+//      mySound = loadSound('assets/doorbell.mp3');
+//      mySound.play();
+//  }
         
 //You are nearing the end of "function setup".
 //Why are you down so far? 
@@ -219,3 +203,64 @@ function draw(){
 //2
 //1
 //BOOOOOOOM
+function draw(){
+    background(51);
+    
+    //stump
+     var colors = color('brown');
+    fill(colors);
+    var stump = rect(615, 360, 70, 70);
+    //draws tree 
+     var c = color('green');
+    fill(c);
+      var tri3 = triangle(460, 370, 650, 200, 830, 370);
+      var thirdColor = color(input5.value);
+        fill(thirdColor);
+        var tri3 = triangle(460, 370, 650, 200, 830, 370);
+          var tri2 = triangle(460, 310, 650, 190, 820, 310);
+          var c = color(input2.value);
+        fill(c);
+        var tri2 = triangle(460, 310, 650, 190, 820, 310);
+    var tri = triangle(478, 250, 650, 130, 800, 250);
+    var color1 = true;
+        var firstColor = color(input4.value);
+        fill(firstColor);
+        var tri = triangle(478, 250, 650, 130, 800, 250);
+    //draws topper
+            var topperColor = color("silver");
+    fill(topperColor);
+    // //creates topper
+    var topper = ellipse(650, 120, 40, 40);
+
+
+    //yellow star
+    if(hasStar === true){
+            var starTopper = loadImage("startopper.png");
+            imageSprite = createSprite(650, 120);
+            imageSprite.addImage(starTopper);
+            drawSprites();
+            
+        }
+        //bluestar
+         if(hasBlueStar === true){
+                var blueStarTopper = loadImage("star2.png");
+                imageSprite = createSprite(650, 120);
+                imageSprite.addImage(blueStarTopper);
+                drawSprites();
+            }
+            //sheep topper
+            if(hasSheep === true){
+                var sheepTopper = loadImage("sheep.png");
+                imageSprite = createSprite(650, 120);
+                imageSprite.addImage(sheepTopper);
+                drawSprites();
+            }
+  
+            //presents
+            if(hasPresents === true){
+                var presents = loadImage("present.png");
+                imageSprite = createSprite(570, 420);
+                imageSprite.addImage(presents);
+                drawSprites();
+            }
+}
